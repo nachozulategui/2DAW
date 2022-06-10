799d66cc37db33f5125e46bf041a90dfc05ee63a
nacho <nachozc69@gmail.com>
2022-06-10 19:06:40 +0200

    cambios enn el react web

--- proyectoreal/src/App.js
+++ proyectoreal/src/App.js
@@ -1,9 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Mensaje from './Mensaje.js';
+
+
 
 function App() {
+
+  
+  
+  const Description = () => {
+    return <p> es la hora de que react me de una respuesta</p>
+  
+  }
+
   return (
     <div className="App">
+      Hola mundo
+      <Mensaje color='green' message='estamos trabayando'/>
+      <Mensaje color='red' message='ahora en un curso'/>
+      <Mensaje color='blue' message='De react'/>
+      <Description/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ proyectoreal/src/Mensaje.js
@@ -0,0 +1,8 @@
+const Mensaje=(params) => {
+
+    console.log(params)
+
+    return <h1 style={{color: props.color }}>{props.message}</h1>
+}
+
+export default Mensaje
